ab1e7a20031bf105ea521adcd9e95df7e4d08d77
kevyngabriel <kevyngabriel.pessoal@gmail.com>
2025-01-27 20:23:28 -0300

    functions

---
+++ rocketseat/funcoes/functions.js
@@ -0,0 +1,15 @@
+// criar um app de frases motivacionais
+
+// function statement or declaration
+function createPhrases() {
+  console.log("Memento Mori - Lembre-se da morte!");
+  console.log("Eterno Prisioneiro da Liberdade!");
+  console.log("A ânsia de ter e o tédio de possuir.");
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases();
+
+console.log("Fim do Programa.");
